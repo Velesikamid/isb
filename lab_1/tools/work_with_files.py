50b323e1462f4b1421929b725c956312a7a791d6
velesikamid <velesikamid05082005@gmail.com>
2025-05-13 13:45:40 +0400

    Added exceptions

--- lab_1/tools/work_with_files.py
+++ lab_1/tools/work_with_files.py
@@ -14,8 +14,11 @@ def read_json(filename: str) -> dict:
     :raises FileNotFoundError: If the file is not found.
     :raises json.JSONDecodeError: If the file contains invalid JSON.
     """
-    with open(filename, "r", encoding="utf-8") as file:
-        return load(file)
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            return load(file)
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 def write_to_json(dictionary: dict, filename: str) -> None:
@@ -26,8 +29,11 @@ def write_to_json(dictionary: dict, filename: str) -> None:
     :param filename: Name of the file to write.
     :raises TypeError: If a non-dictionary is passed.
     """
-    with open(filename, "w", encoding="utf-8") as file:
-        dump(dictionary, file, ensure_ascii=False, indent=4)
+    try:
+        with open(filename, "w", encoding="utf-8") as file:
+            dump(dictionary, file, ensure_ascii=False, indent=4)
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 def read_txt(filename: str) -> str:
@@ -38,8 +44,11 @@ def read_txt(filename: str) -> str:
     :return: String with the file contents.
     :raises FileNotFoundError: If the file is not found.
     """
-    with open(filename, "r", encoding="utf-8") as file:
-        return file.read()
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            return file.read()
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 def write_to_txt(filename: str, data: str) -> None:
@@ -50,5 +59,8 @@ def write_to_txt(filename: str, data: str) -> None:
     :param data: String to write to the file.
     :raises TypeError: If a non-string is passed.
     """
-    with open(filename, "w", encoding="utf-8") as file:
-        file.write(data)
+    try:
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(data)
+    except Exception as e:
+        print(f"Error: {e}")
